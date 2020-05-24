35657454373a1b7f80a385fa62f5547fafe30ac9
Naci Akce <n.akce@limesoda.com>
2020-05-24 22:09:56 +0200

    Add Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,9 +5,8 @@ import './Loader.scss';
 const Loader: React.FC = () => {
 
     return <>
-        <h1 className="app__title">Loading</h1>
         <div className="loader__container">
-            <div className="loader"><div></div><div></div><div></div><div></div></div>
+            <div data-testid="loader" className="loader"><div></div><div></div><div></div><div></div></div>
         </div>
     </>;
 };
--- src/components/Loader/__tests__/Loader.tsx
+++ src/components/Loader/__tests__/Loader.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import Loader from '../Loader';
 
-afterEach(cleanup);
-
 describe('Loader', () => {
+    afterEach(cleanup);
+    
     it('renders without crashing', () => {
         const { asFragment } = render(<Loader />);
         expect(asFragment()).toMatchSnapshot();
