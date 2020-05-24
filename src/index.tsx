5c8fea1bd8f8297e0f4ba26f4adc306b92ddbd16
Naci Akce <n.akce@limesoda.com>
2020-05-24 22:13:53 +0200

    Add redux Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import App from './App';
+import Store from './redux/store';
 
 import './index.scss';
-import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={Store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
